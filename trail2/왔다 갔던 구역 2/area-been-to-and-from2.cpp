837bb71236da13d7ae0af7158760858865ae79b0
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-05 01:01:01 +0900

    [Passed] 왔다 갔던 구역 2 (1ms, 0MB)

--- trail2/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ trail2/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -1,52 +1,59 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+
+#define MAX_N 100
+#define MAX_R 2000
+#define OFFSET 1000
 
 using namespace std;
 
-const int OFFSET = 10000;
-const int MAX_R = 20005;
+int n;
+int x1[MAX_N], x2[MAX_N];
 
-int diff[MAX_R];
+int checked[MAX_R + 1];
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int n;
+    // 입력
     cin >> n;
-
+    
+    // 현재 위치
     int cur = 0;
-
-    for (int i = 0; i < n; i++) {
-        int dist;
-        char dir;
-        cin >> dist >> dir;
-
-        int nxt;
-        if (dir == 'R') {
-            nxt = cur + dist;
-            diff[cur + OFFSET] += 1;
-            diff[nxt + OFFSET] -= 1;
-        } else {
-            nxt = cur - dist;
-            diff[nxt + OFFSET] += 1;
-            diff[cur + OFFSET] -= 1;
+    
+    for(int i = 0; i < n; i++) {
+        int distance;
+        char direction;
+        cin >> distance >> direction;
+        
+        if(direction == 'L') {
+            // 왼쪽으로 이동할 경우 : cur - distance ~ cur까지 경로 이동
+            x1[i] = cur - distance;
+            x2[i] = cur;
+            cur -= distance;
         }
-        cur = nxt;
-    }
-
-    int ans = 0;
-    int current_overlap = 0;
-
-    for (int i = 0; i < MAX_R - 1; i++) {
-        current_overlap += diff[i];
-        if (current_overlap >= 2) {
-            ans++;
+        else {
+            // 오른쪽으로 이동할 경우 : cur ~ cur + distance까지 경로 이동
+            x1[i] = cur;
+            x2[i] = cur + distance;
+            cur += distance;
         }
+        
+        // OFFSET을 더해줍니다.
+        x1[i] += OFFSET;
+        x2[i] += OFFSET;
     }
-
-    cout << ans << '\n';
-
+    
+    // 구간을 칠해줍니다.
+    // 구간 단위로 진행하는 문제이므로
+    // x2[i]에 등호가 들어가지 않음에 유의합니다.
+    for(int i = 0; i < n; i++)
+        for(int j = x1[i]; j < x2[i]; j++)
+            checked[j]++;
+    
+    // 2번 이상 지나간 영역의 크기를 구합니다.
+    int cnt = 0;
+    for(int i = 0; i <= MAX_R; i++)
+        if(checked[i] >= 2)
+            cnt++;
+    
+    cout << cnt << '\n';
     return 0;
 }
