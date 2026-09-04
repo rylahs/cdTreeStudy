608b88362a73725b0fcc92a0da59aad2787a5fd4
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-05 00:57:52 +0900

    [Wrong Answer] 왔다 갔던 구역 2 (1ms, 0MB)

---
+++ trail2/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    vector<int> v(2002);
+
+    int mark = 1000;
+
+    while(n--)
+    {
+        int x;
+        char cmd;
+        cin >> x >> cmd;
+
+        if (cmd == 'L')
+        {
+            while (x--)
+            {
+                v[mark]++;
+                mark--;
+            }
+        }
+
+        else
+        {
+            while (x--)
+            {
+                v[mark]++;
+                mark++;
+            }
+        }
+    }
+    int ans = 0;
+    for (auto& e : v)
+    {
+        if (e >= 2)
+            ans++;
+    }
+
+    cout << ans << '\n';
+
+    
+    return 0;
+}
