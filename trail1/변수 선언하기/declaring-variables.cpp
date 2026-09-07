b26a515ece25a89850d92f4a6367db2b60751b8d
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-07 11:49:53 +0900

    [Passed] 변수 선언하기 (2ms, 0MB)

---
+++ trail1/변수 선언하기/declaring-variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 3;
+    char b = 'C';
+
+    cout << a << '\n';
+    cout << b << '\n';
+    
+    return 0;
+}
