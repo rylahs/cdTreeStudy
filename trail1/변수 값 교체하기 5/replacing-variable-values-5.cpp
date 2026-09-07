53f668f5fb85a11990623300e5c727121a80c6f4
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-07 13:28:46 +0900

    [Passed] 변수 값 교체하기 5 (2ms, 0MB)

---
+++ trail1/변수 값 교체하기 5/replacing-variable-values-5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 2;
+    int b = 6;
+
+    a = b;
+
+    cout << a << '\n';
+    cout << b << '\n';
+    
+    return 0;
+}
