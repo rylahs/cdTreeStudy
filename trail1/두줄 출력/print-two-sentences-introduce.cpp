41393ac885800f28757c5592c059ff50ddc69c16
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-07 11:37:29 +0900

    [Passed] 두줄 출력 (2ms, 0MB)

---
+++ trail1/두줄 출력/print-two-sentences-introduce.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    cout << "Hello students!\n";
+    cout << "Welcome to LeebrosCode!\n";
+    
+    return 0;
+}
