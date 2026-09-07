0f8eb51c496242809b7d52dc8e8a055313c28898
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-07 23:56:31 +0900

    [Passed] 변수 값 복사하기 2 (1ms, 0MB)

---
+++ trail1/변수 값 복사하기 2/copying-variable-values-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 5, b = 6, c = 7;
+    a = c;
+    b = c;
+
+    cout << a << ' ' << b << ' ' << c << '\n';
+    return 0;
+}
