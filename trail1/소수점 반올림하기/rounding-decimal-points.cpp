3ea8ba19f14c25a479da53920a8b05c6430e7f2a
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-07 12:44:11 +0900

    [Passed] 소수점 반올림하기 (2ms, 0MB)

---
+++ trail1/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    double a = 25.352;
+
+    cout << fixed;
+    cout << setprecision(1);
+
+    cout << a << '\n';
+    return 0;
+}
