557bd48e6d87dc2926de1d1b25e35d6e7667456c
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-07 16:19:15 +0900

    [Passed] 정수 복사 (2ms, 0MB)

---
+++ trail1/정수 복사/copy-integer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 3;
+    int b = 4;
+
+    b = a;
+
+    cout << a << ' ' << b << '\n';
+    cout << a * b << '\n';
+    
+    return 0;
+}
