24721382563fddff48db6c4438335974e41e6500
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-08 19:38:04 +0900

    [Passed] 입력받아 계산 4 (2ms, 0MB)

---
+++ trail1/입력받아 계산 4/input-calculate-4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+
+    a = a << 1;
+
+    cout << a << '\n';
+    return 0;
+}
