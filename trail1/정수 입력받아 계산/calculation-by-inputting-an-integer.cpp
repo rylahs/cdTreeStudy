f24e852c436378e1699c11b561b4f4484416f26a
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-08 19:38:47 +0900

    [Passed] 정수 입력받아 계산 (2ms, 0MB)

---
+++ trail1/정수 입력받아 계산/calculation-by-inputting-an-integer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a;
+    cin >> a;
+
+    a = (a << 1) + 3;
+
+    cout << a << '\n';
+
+    return 0;
+}
