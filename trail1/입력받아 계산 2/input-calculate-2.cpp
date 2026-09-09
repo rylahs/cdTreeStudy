2e2572f577f157e5e55f86eeb1be8cde487d6a02
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-09 19:41:11 +0900

    [Passed] 입력받아 계산 2 (2ms, 0MB)

---
+++ trail1/입력받아 계산 2/input-calculate-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+
+    int res = a * b;
+
+    cout << a * b << '\n';
+    return 0;
+}
