b2214f4a60f3bca33bf69df7408c21a370bd8748
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-09 19:59:40 +0900

    [Passed] 입력받아 출력 2 (2ms, 0MB)

---
+++ trail1/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int birth, num;
+    cin >> birth;
+    cin.get();
+    cin >> num;
+
+    cout << birth << num << '\n';
+    return 0;
+}
