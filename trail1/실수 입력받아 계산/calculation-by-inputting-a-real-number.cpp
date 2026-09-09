e57a1f690c0c32333148d5b8d9dd757d16177dab
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-09 19:46:18 +0900

    [Passed] 실수 입력받아 계산 (2ms, 0MB)

---
+++ trail1/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a, b;
+    cin >> a >> b;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << a + b << '\n';
+    return 0;
+}
