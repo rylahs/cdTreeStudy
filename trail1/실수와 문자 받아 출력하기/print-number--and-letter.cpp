af69992251e52b220a2b8005293e9b7e247eb7a1
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-09 19:49:58 +0900

    [Passed] 실수와 문자 받아 출력하기 (1ms, 0MB)

---
+++ trail1/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char c;
+    double a, b;
+    cin >> c;
+    cin >> a >> b;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << c << '\n';
+    cout << a << '\n';
+    cout << b << '\n';
+    return 0;
+}
