0acc56a3b64bb0df48c4bb406cfcc8c1ac4b41f9
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-09 13:39:04 +0900

    [Passed] 실수 받아 그대로 출력 (2ms, 0MB)

---
+++ trail1/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double n;
+    cin >> n;
+
+    cout << fixed;
+    cout << setprecision(2);
+
+    cout << n << '\n';
+    return 0;
+}
