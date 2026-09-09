db99a8fa0cca5baa0ec12f9654ab493b9b0be85c
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-09 19:46:59 +0900

    [Passed] 세 실수의 반올림 (2ms, 0MB)

---
+++ trail1/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a, b, c;
+    cin >> a >> b >> c;
+
+    cout << fixed;
+    cout.precision(3);
+
+    cout << a << '\n';
+    cout << b << '\n';
+    cout << c << '\n';
+    return 0;
+}
