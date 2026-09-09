ee2726524f3b7c40215fedaf75392a6b7ded74ce
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-09 19:51:31 +0900

    [Passed] 문자열 순서 바꾸기 (2ms, 0MB)

---
+++ trail1/문자열 순서 바꾸기/change-order-of-strings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string s, t;
+    cin >> s >> t;
+
+    cout << t << '\n';
+    cout << s << '\n';
+    
+    return 0;
+}
