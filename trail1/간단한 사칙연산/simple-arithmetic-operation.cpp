ff8ee8c334cbde0017f8b6f36df3fd27d69a7413
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-10 11:24:24 +0900

    [Passed] 간단한 사칙연산 (1ms, 0MB)

---
+++ trail1/간단한 사칙연산/simple-arithmetic-operation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a, b;
+    cin >> a >> b;
+
+    cout << a + b << '\n';
+    cout << a - b << '\n';
+    cout << a / b << '\n';
+    cout << a % b << '\n';
+    return 0;
+}
