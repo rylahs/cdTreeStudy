3528b7b552abf0e8243cf86c3ead6842e65c0d47
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-10 11:23:22 +0900

    [Wrong Answer] 전화번호 바꾸기 (2ms, 0MB)

---
+++ trail1/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int f, s, t;
+    cin >> f;
+    cin.get();
+    cin >> s;
+    cin.get();
+    cin >> t;
+
+    cout << 010 << '-' << t << '-' << s << '\n';
+
+    return 0;
+}
