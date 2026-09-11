67ff8c0cc324ce3bab55f96c73024a0080270718
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-11 23:41:56 +0900

    [Passed] 음수 구별하기 (2ms, 0MB)

---
+++ trail1/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+
+    cout << a << '\n';
+    if (a < 0)
+        cout << "minus\n";
+    return 0;
+}
