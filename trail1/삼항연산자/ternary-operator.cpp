81fa84aa02931f1ec924063a49ae16f5f7aacb99
Rylah <94848571+rylahs@users.noreply.github.com>
2026-09-20 00:53:30 +0900

    [Passed] 삼항연산자 (2ms, 0MB)

---
+++ trail1/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int score;
+    cin >> score; 
+
+    cout << (score == 100 ? "pass\n" : "failure\n");
+    return 0;
+}
